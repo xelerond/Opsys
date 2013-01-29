9ffe02b02f10f022a0090dad69fb237ff3ed52f0
splittie96 <robbie.gavin.henderson@gmail.com>
2013-01-29 22:39:52 +0000

    finished with loop

--- problem31/problem3.c
+++ problem31/problem3.c
@@ -3,8 +3,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
-void bufferiser(int fd)
+//function provided by Bodo on lab sheet
+char* bufferiser(int fd)
 {
     char* buf = NULL;
     char* bufp;
@@ -13,66 +15,97 @@ void bufferiser(int fd)
     struct stat st;
     /* Assuming that FD is a file descriptor opened for
     reading, get file system I/O blocksize into BUFSZ. */
-    if (fstat (fd, &st) >= 0) {
-    bufsz = (size_t) st.st_blksize;
-    } else {
+    
+    if (fstat (fd, &st) >= 0)
+    {
+        bufsz = (size_t) st.st_blksize;
+    }
+    
+    else
+    {
         puts("HELLO");
-    /* Handle error. */
+        /* Handle error. */
     }
     /* Allocate buffer of size BUFSZ. */
     buf = (char *) malloc (bufsz);
     curpos = 0;
     cursz = bufsz;
     /* Block read FD, storing data into BUF. */
-    while ((ssz = read (fd, buf + curpos, bufsz)) > 0) {
-    curpos += ssz;
-    cursz = curpos + bufsz;
-    if (NULL == (bufp = (char *) realloc (buf, cursz))) {
-    /* Handle error. */
-    }
-    buf = bufp;
+    while ((ssz = read (fd, buf + curpos, bufsz)) > 0)
+    {
+        curpos += ssz;
+        cursz = curpos + bufsz;
+        if (NULL == (bufp = (char *) realloc (buf, cursz)))
+        {
+            /* Handle error. */
+        }
+        buf = bufp;
     }
     /* Zero-terminate BUF. */
     buf[curpos] = 0;
+    return(buf);
+}
 
+int fileCount(char* lsReturn, int max)
+{
+    char current = lsReturn[0];
+    int i = 0;
+    int files = 0;
     
-
-
-    puts(buf);
+    while(files <= max +2)
+    {
+        if (current == '\n')
+            files++;
+        else if (current == '\0')
+            return files;
+        i++;
+        current = lsReturn[i];
+    }
+    if (files > max)
+        return max+2;
+    else
+        return files;
+    
+    return 0;
 }
 
 
-int main(int argc, char * argv[])
+int main(int argc, char** argv)
 {
-    int pid;
-    pid = fork();
+    int max = 25;
     int fd[2];
-    pipe(fd);
-    
+    char* buffer;
+    int pid;
+    pipe(fd);      
+    pid = fork();
+
     switch(pid)
-    {
-        case -1:
         {
-            perror("fork error");
-            exit(1);
-            break;
-        }
         case 0:
-        {
             close(fd[0]);
-            close(0);
-            dup2(fd[1],0);
+            close(1);
+            dup2(fd[1],1);
             execv("/bin/ls", argv);
-            break;
-        }
+        break;
+
+        case -1:
+            printf("FAILED");
+        break;
+
         default:
-        {
             wait(&pid);
             close(fd[1]);
-            close(1);
-            bufferiser(fd[0]);
-            break;
-        }
+            close(0);
+            buffer = bufferiser(fd[0]);
+            if(fileCount(buffer, max)<max+1)
+            {
+                puts(buffer);
+            }
+            else
+            {
+                puts("too many files");
+            }
+        break;
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
