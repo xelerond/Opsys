1a2f47e114071ce4ca30496a5932b2f5f90d743b
splittie96 <robbie.gavin.henderson@gmail.com>
2013-01-29 23:01:43 +0000

    makefiles added

--- problem21/problem21.c
+++ problem21/problem21.c
@@ -6,23 +6,22 @@
 void printtime()
 {
 	time_t t;
-		struct tm *lat;
-		t = time(NULL);
-		lat = localtime(&t);
-		openlog ("time program", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-		syslog(LOG_INFO, "Local time is : %s", asctime(lat));
-		printf("Local time is : %s \n", asctime(lat));
-		closelog();
+	struct tm *lat;
+	t = time(NULL);
+	lat = localtime(&t);
+	openlog ("time program", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+	syslog(LOG_INFO, "Local time is : %s", asctime(lat));
+	//printf("Local time is : %s", asctime(lat));
 }
 
 void timedprint()
 {
 	int cake = 0;
-	while (cake<1000)
+	while (cake<10)
 	{
-		sleep(10);
 		printtime();
-		cake ++;
+		sleep(10);
+		//cake++;
 	}
 }
 
