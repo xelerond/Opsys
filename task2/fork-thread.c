95156f7fbd6bc428df01ccefc6ee78c957a71f3f
root <root@ubuntu.ubuntu-domain>
2013-02-11 21:55:18 +0000

    nearly done threads and forks just needs some stuff checked

--- task2/fork-thread.c
+++ task2/fork-thread.c
@@ -40,7 +40,7 @@ void measure_forks (unsigned number)
 		switch(cur_pid)
 		{
 			case 0:
-				dummy(0);
+				dummy(NULL);
 				pid[number] = cur_pid;
 			case 1:
 				exit(0);
@@ -71,6 +71,38 @@ void measure_threads (unsigned number)
     /* TODO Modify MEASURE_FORKS, replacing FORK
             with thread creation and WAIT with
             thread join.  */
+    struct timespec start, stop, finish;
+    unsigned i = 0;
+    int thread;
+    pthread_t tid [number];
+    pthread_t cur_tid;
+
+    clock_gettime(CLOCK_REALTIME, &start);
+    for (i = 0;i < number; i++) 
+    {
+		
+		thread = pthread_create(&cur_tid, NULL, dummy, NULL);
+		if(thread != 0)
+		{
+			puts("Thread could not be created");
+		}
+		tid[number] = cur_tid;
+		
+	}
+    
+    clock_gettime(CLOCK_REALTIME, &stop);
+
+    for (i = 0; i < number; i++) {
+		
+		pthread_join(tid[number], NULL);
+		
+    }
+    clock_gettime(CLOCK_REALTIME, &finish);
+
+    printf ("process: num=%03u, fork=%03li, wait=%03li, totatl=%03li\n",
+            number, xelapsed (stop, start), xelapsed (finish, stop),
+            xelapsed (finish, start));        
+    
 }
 
 
@@ -80,7 +112,8 @@ main (int argc, char *argv[])
     /* TODO Get a number of instances from the argument list
        TODO Check that the arguments are valid
        TODO Replace the argument in the subsequent function calls.  */
+    
     measure_forks (atoi(argv[1]));
-    measure_threads (1);
+    measure_threads (atoi(argv[1]));
     return EXIT_SUCCESS;
 }
