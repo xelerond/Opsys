61d7c7a01307cd1cfa96c7630bace459f5fd6833
splittie96 <robbie.gavin.henderson@gmail.com>
2013-03-06 23:12:15 +0000

    changed test

--- task3/problem1.1/testpart3.c
+++ task3/problem1.1/testpart3.c
@@ -3,21 +3,9 @@
 #include <stdio.h>
 int main()
 {
-	int counter;
-	void * pointer;
-	int d = 1;
-	for(counter = 1; counter > 0; counter += d)
-	{
-		pointer = super_malloc(sizeof(int)*counter);
-		printf("%lu\n", (size_t)pointer);
-		if(pointer != NULL)
-		{
-			super_mfree(pointer);
-			if(counter == 10)
-			{
-				d = -1;
-			}
-		}
-	}
+	void * pointer = super_malloc(sizeof(char));
+	printf("allocating memory....\naddress: %li\n\n", (size_t)pointer);
+	pointer = super_mfree(pointer);
+	printf("freeing memory....\naddress: %li\n", (size_t)pointer);
 	return 0;
 }
